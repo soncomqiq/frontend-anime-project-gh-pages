08fca256f6a19946878c4359240701d5a9e74ea3
soncomqiq <idgamescs@hotmail.com>
2024-04-01 21:47:26 +0700

    Edit Action

--- src/App.tsx
+++ src/App.tsx
@@ -8,32 +8,35 @@ import Favorites from "./favorites";
 const BASE_URL = "/frontend-anime-project-gh-pages";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: BASE_URL + "/",
-      element: <Home />,
-    },
-    {
-      path: BASE_URL + "/detail/:mal_id",
-      element: <Detail />,
-    },
-    {
-      path: BASE_URL + "/seasons",
-      element: <Seasons />,
-    },
-    {
-      path: BASE_URL + "/favorites",
-      element: <Favorites />,
-    },
-    {
-      path: BASE_URL + "/favorites/detail/:mal_id",
-      element: <Detail />,
-    },
-    {
-      path: BASE_URL + "/seasons/detail/:mal_id",
-      element: <Detail />,
-    },
-  ], {basename: BASE_URL+"/"});
+  const router = createBrowserRouter(
+    [
+      {
+        path: BASE_URL + "/",
+        element: <Home />,
+      },
+      {
+        path: BASE_URL + "/detail/:mal_id",
+        element: <Detail />,
+      },
+      {
+        path: BASE_URL + "/seasons",
+        element: <Seasons />,
+      },
+      {
+        path: BASE_URL + "/favorites",
+        element: <Favorites />,
+      },
+      {
+        path: BASE_URL + "/favorites/detail/:mal_id",
+        element: <Detail />,
+      },
+      {
+        path: BASE_URL + "/seasons/detail/:mal_id",
+        element: <Detail />,
+      },
+    ],
+    { basename: BASE_URL }
+  );
   return <RouterProvider router={router} />;
 }
 
